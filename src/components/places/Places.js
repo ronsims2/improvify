139f7d543dce61679fcde4e1a0f03324e2155532
Ronald Sims II <ron@orangemantis.net>
2019-10-01 20:33:07 -0400

    Started on recalc logic.

--- src/components/places/Places.js
+++ src/components/places/Places.js
@@ -2,9 +2,28 @@ import React, { useState, useRef } from 'react'
 import Bucket from "../bucket/Bucket";
 
 const Places = () => {
-    const initialTotal = Math.ceil(Math.random() * 10000000)
-    const [total, setTotal] = useState(initialTotal)
-    const recalc = (num) => setTotal(num + total)
+    const initialTotal = String(Math.ceil(Math.random() * 10000000))
+    const [total, setTotal] = useState(initialTotal.split())
+    const recalc = (num, place) => {
+        const pos = place.length - 1
+
+        const workingTotalArray = [...total].reverse()
+        let workingTotal = 0
+
+        workingTotalArray.forEach((item, i) => {
+            let itemAmount = parseInt(item)
+            if (i === pos) {
+                //add in increase
+                itemAmount += num
+            }
+
+            // Use Multiply
+            itemAmount = itemAmount * parseInt(`1${Array(pos).fill('0').join('')}`)
+
+            workingTotal += itemAmount
+        })
+
+    }
 
     return (
         <>
