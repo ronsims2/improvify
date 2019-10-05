f3703254db8c2c280888723486321366cec72669
Ronald Sims II <ron@orangemantis.net>
2019-10-05 15:09:18 -0400

    added a new build with a home page and a place values activity.

---
+++ precache-manifest.7ef2b6769b13dae9c0de88dda1f9666c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f71b5191796668fccebd4327be36e22",
+    "url": "/improvify/index.html"
+  },
+  {
+    "revision": "3b7d5d0951788ecb9e7c",
+    "url": "/improvify/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0cc66301d1b2c934a9f1",
+    "url": "/improvify/static/css/main.3984df87.chunk.css"
+  },
+  {
+    "revision": "3b7d5d0951788ecb9e7c",
+    "url": "/improvify/static/js/2.9409f93a.chunk.js"
+  },
+  {
+    "revision": "0cc66301d1b2c934a9f1",
+    "url": "/improvify/static/js/main.e3ce8cf6.chunk.js"
+  },
+  {
+    "revision": "31717120b7aee19bc1b7",
+    "url": "/improvify/static/js/runtime-main.bceb51ce.js"
+  }
+]);
